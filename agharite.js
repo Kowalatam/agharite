ee5a8ca41c46d3862a2a3a9d345aadf53b4f78d2
Elizabeth Paul <elizapaul01@gmail.com>
2018-04-27 03:20:33 +0200

    First upload

---
+++ agharite.js
@@ -0,0 +1,14 @@
+// var images=
+// 	[
+// 		'http://omofuomaagharite.com/wp-content/uploads/2018/04/SOLITUDE-1600x1013.jpg',
+// 		'http://omofuomaagharite.com/wp-content/uploads/2018/04/THE-GATHERING-1600x1010.jpg',
+// 		'http://omofuomaagharite.com/wp-content/uploads/2018/04/SKIES-MAJESTIC-1600x1200.jpg',
+// 		'http://omofuomaagharite.com/wp-content/uploads/2018/04/PROCESS-1600x1200.jpg',
+// 		'http://omofuomaagharite.com/wp-content/uploads/2018/04/THE-PROCESS-OF-NATURE.jpg'
+// 	];
+
+// setInterval(function(){
+//   var url=images[Math.floor(Math.random() * images.length)];
+//   $('.main-text').css('background-image', 'url(' + url + ')');
+//   // document.getElementsByClassName('main-text').style.backgroundImage = 'url('+url+')';  
+// },5000);
